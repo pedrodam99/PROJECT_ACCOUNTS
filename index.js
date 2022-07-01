90a4c45d79a0e6c4df1cb5879b9485fa5d31b169
Pedro Dambrosio <pedrodam99@gmail.com>
2022-07-01 15:19:36 -0300

    Criacao de conta

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function operation() {
         message: "O que voce deseja?",
         choices: [
           "Criar conta",
-          "Consultar Saldo",
+          "Consultar saldo",
           "Depositar",
           "Sacar",
           "Sair",
@@ -26,7 +26,51 @@ function operation() {
     ])
     .then((answer) => {
       const action = answer["action"];
-      console.log(action);
+      if (action == "Criar conta") createAcccount();
     })
     .catch((err) => console.log(err));
+}
+
+//criando a conta
+function createAcccount() {
+  console.log(
+    chalk.bgGreen.black(`Parabéns! Obrigado por escolher o nosso banco`)
+  );
+  console.log(chalk.green("Defina as opções da sua conta a seguir"));
+  buildAccount();
+}
+
+function buildAccount() {
+  inquirer
+    .prompt([
+      { name: "accountName", message: "Digite um nome para sua conta?" },
+    ])
+    .then((answer) => {
+      const accountName = answer["accountName"];
+      console.info(accountName);
+
+      if (!fs.existsSync("accounts")) {
+        fs.mkdirSync("accounts");
+      }
+
+      if (fs.existsSync(`accounts/${accountName}.json`)) {
+        console.log(
+          chalk.bgRed.black("Esse nome já existe, escolha outro nome")
+        );
+        buildAccount();
+        return;
+      }
+
+      fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        '{"balance": 0}',
+        function (err) {
+          console.log(err);
+        }
+      );
+
+      console.log(chalk.green("Parabéns, sua conta foi criada!"));
+      operation();
+    })
+    .catch((err) => err);
 }
